f98b4e93319447ca2a8d9711c2c6e1c6ee938182
patvibh <pateshwari.vibha@gmail.com>
2023-05-05 16:16:03 +0530

    Update data

--- sql_code.sql
+++ sql_code.sql
@@ -1,13 +1,12 @@
 --- Title :-        Retail Transactions
---- Created by :-   Hardi Jain & Vinit Sangoi
---- Date :-         20-09-2022
+--- Created by :-   Vibha Pateshwari
+--- Date :-         12-04-2023
 --- Tool used:-     PostgreSQL
 
 /*
 Description :- 
 		• This is a Retail Transations SQL Project. This database contains 1 tables retail.
 		• Retail table has 9000 rows.
-		• In this project, Hardi Jain & Vinit Sangoi worked together.
 		
 Approach :- 
 		• Understanding the dataset
